36843dc5e6c4620b5d902e499394c598110e5a36
Soichiro Ota <libarta.official@gmail.com>
2020-01-04 03:08:47 +0900

    update model

--- doc2color/preprocess.py
+++ doc2color/preprocess.py
@@ -21,19 +21,39 @@ def preprocess(name_series, method):
     return name_series
 
 
+def get_name_hex_rgb_df(df):
+    return df[['Name', 'Hex (RGB)']]
+
+
 if __name__ == '__main__':
-    df = pd.read_csv('doc2color/data/color.csv')
+    hex_rgb = 'Hex (RGB)'
+    main_df = pd.read_csv('doc2color/data/color.csv')
+    df1 = pd.read_csv('doc2color/data/crayon1.csv').rename(columns={'Hexadecimal in their website depiction[b]': hex_rgb})
+    df2 = pd.read_csv('doc2color/data/crayon2.csv').rename(columns={'Hexadecimal': hex_rgb})
+    df5 = pd.read_csv('doc2color/data/crayon5.csv').rename(columns={'Hexadecimal': hex_rgb})
+    df15 = pd.read_csv('doc2color/data/crayon15.csv').rename(columns={'Hex Code': hex_rgb})
+    df17 = pd.read_csv('doc2color/data/crayon17.csv').rename(columns={'Hex Code': hex_rgb})
+    scent_df17 = df17.rename(columns={'Scent Name': 'Name'})
+    color_df17 = df17.rename(columns={'Color Name': 'Name'})
+    df19 = pd.read_csv('doc2color/data/crayon19.csv').rename(columns={'Hex Code': hex_rgb})
+    color_dic_df = pd.read_csv('doc2color/data/color_dic.csv')
+
+    concat_df = pd.concat([get_name_hex_rgb_df(d) for d in (main_df, df1, df2, df5, df15, scent_df17, color_df17, df19, color_dic_df)]).dropna()
+    df = concat_df[concat_df['Hex (RGB)'] != 'unknown']
 
     rgb_dicts = []
-    for rgb in df['Hex (RGB)']:
+    for i, row in df.iterrows():
+        rgb = row[hex_rgb]
+        print(row['Name'], rgb)
         rgb_dict = {
             'R': int(rgb[1:3], 16),
             'G': int(rgb[3:5], 16),
-            'B': int(rgb[5:], 16)
+            'B': int(rgb[5:7], 16)
         }
         print(rgb, rgb_dict)
         rgb_dicts.append(rgb_dict)
     rgb_df = pd.DataFrame(rgb_dicts)
+    print(rgb_df.shape)
     rgb_df.to_csv('doc2color/data/rgb.csv', sep=',', index=False)
 
     model = BertModelWithTokenizer("bert-base-multilingual-cased")
--- doc2color/scraper.py
+++ doc2color/scraper.py
@@ -21,6 +21,38 @@ def scrape(url):
         return None
 
 
+def scrape_crayon(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    response.encoding = response.apparent_encoding
+    dfs = pd.read_html(response.text)
+    for i, df in enumerate(dfs):
+        print(df.head(), df.columns, df.shape, i)
+    for n in (1, 2, 5, 15, 17, 19):
+        dfs[n].to_csv('doc2color/data/crayon{}.csv'.format(n), sep=',', index=False)
+
+
+def scrape_color_dic(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    response.encoding = response.apparent_encoding
+    html = lxml.html.fromstring(response.text)
+    names1 = html.xpath('//table[@class="colortable"]//td/a/text()[1]')
+    names2 = html.xpath('//table[@class="colortable"]//td/a/span/text()')
+    color_codes = html.xpath('//table[@class="colortable"]//td/a/text()[2]')
+    print(names1, names2, color_codes)
+    dicts = []
+    for i in range(len(names1)):
+        record = dict(name=names1[i], color_code=color_codes[i])
+        dicts.append(record)
+    for i in range(len(names2)):
+        record = dict(name=names2[i], color_code=color_codes[i])
+        dicts.append(record)
+    if dicts:
+        return pd.DataFrame(dicts).rename(columns={'name': 'Name', 'color_code': 'Hex (RGB)'})
+    return None
+
+
 if __name__ == '__main__':
     urls = (
         'https://en.wikipedia.org/wiki/X11_color_names',
@@ -31,3 +63,16 @@ if __name__ == '__main__':
     df = pd.concat([d for d in dfs if d is not None])
     print(df.head(), df.columns, df.shape)
     df.to_csv('doc2color/data/color.csv', sep=',', index=False)
+
+    crayon_url = 'https://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors'
+    scrape_crayon(crayon_url)
+
+    color_dic_urls = (
+        'https://www.colordic.org/',
+        'https://www.colordic.org/w',
+        'https://www.colordic.org/y',
+        'https://www.colordic.org/m')
+    color_dic_dfs = [scrape_color_dic(url) for url in color_dic_urls]
+    color_dic_df = pd.concat([d for d in color_dic_dfs if d is not None])
+    print(color_dic_df.head(), color_dic_df.columns, color_dic_df.shape)
+    color_dic_df.to_csv('doc2color/data/color_dic.csv', sep=',', index=False)
